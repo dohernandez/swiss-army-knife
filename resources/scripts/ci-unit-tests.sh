6e98ef19b6228573a84b465f6f2e5e64a91c3d0f
Darien Hernández González <dohernandez@gmail.com>
2019-04-02 12:59:14 +0200

    Skip jobs when env variable is not defined.

--- resources/scripts/ci-unit-tests.sh
+++ resources/scripts/ci-unit-tests.sh
@@ -11,6 +11,12 @@ make deps-vendor
 # Running tests
 make test
 
+
+if [ "${CODECOV_TOKEN}" == "" ]; then
+    echo "No CODECOV_TOKEN defined, upload coverage results. Skipping"
+    exit 0
+fi
+
 # Upload coverage results
 # Example https://docs.coveralls.io/go
 $GOPATH/bin/goveralls -coverprofile=overalls.coverprofile -service=travis-ci -repotoken $CODECOV_TOKEN
